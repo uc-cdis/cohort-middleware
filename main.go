766f4a03178120c3bc9fa82caad1464be1fbe1f1
pieterlukasse <pieterlukasse@gmail.com>
2022-03-24 17:21:33 +0100

    improve usage message

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"flag"
+
 	"github.com/uc-cdis/cohort-middleware/config"
 	"github.com/uc-cdis/cohort-middleware/db"
 	"github.com/uc-cdis/cohort-middleware/server"
 )
 
 func main() {
-	environment := flag.String("e", "development", "")
+	environment := flag.String("e", "development", "Environment/prefix of config file name")
+	flag.Parse()
 	config.Init(*environment)
 	db.Init()
 	server.Init()

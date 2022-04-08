1589474c29280ce98276189b1ca4d0806e607ea4
pieterlukasse <pieterlukasse@gmail.com>
2022-04-08 16:34:48 +0200

    more cleanup

--- models/cohort.go
+++ models/cohort.go
@@ -1,21 +1,4 @@
 package models
 
-import (
-	"time"
-
-	"github.com/uc-cdis/cohort-middleware/db"
-)
-
 type Cohort struct {
-	CohortDefinitionId int `json:",omitempty"`
-	SubjectId          int64
-	CohortStartDate    time.Time
-	CohortEndDate      time.Time
-}
-
-func (h Cohort) GetCohortById(id int) ([]*Cohort, error) {
-	db2 := db.GetAtlasDB().Db
-	var cohort []*Cohort
-	db2.Model(&Cohort{}).Select("cohort_definition_id, subject_id, cohort_start_date, cohort_end_date").Where("cohort_definition_id = ?", id).Scan(&cohort)
-	return cohort, nil
 }

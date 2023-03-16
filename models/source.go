143ed288e9b5282f00956c33b3f10c31fa74cecc
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2023-03-16 12:58:01 -0500

    debug: extra echo

--- models/source.go
+++ models/source.go
@@ -17,6 +17,7 @@ type Source struct {
 
 func (h Source) GetSourceById(id int) (*Source, error) {
 	db2 := db.GetAtlasDB().Db
+	fmt.Println(db2.Exec("SELECT * FROM source;")
 	var dataSource *Source
 	db2.Model(&Source{}).
 		Select("source_id, source_name").
@@ -27,6 +28,7 @@ func (h Source) GetSourceById(id int) (*Source, error) {
 
 func (h Source) GetSourceByIdWithConnection(id int) (*Source, error) {
 	db2 := db.GetAtlasDB().Db
+	fmt.Println(db2.Exec("SELECT * FROM source;"))
 	var dataSource *Source
 	db2.Model(&Source{}).
 		Select("source_id, source_name, source_connection, source_dialect, username, password").
@@ -42,6 +44,7 @@ type SourceSchema struct {
 func (h Source) GetSourceSchemaNameBySourceIdAndSourceType(id int, sourceType SourceType) (*SourceSchema, error) {
 	atlasDb := db.GetAtlasDB()
 	db2 := atlasDb.Db
+	fmt.Println(db2.Exec("SELECT * FROM source;")
 	var sourceSchema *SourceSchema
 	db2.Model(&Source{}).
 		Select("source_daimon.table_qualifier as schema_name").

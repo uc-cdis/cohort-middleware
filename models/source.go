d531a32c38a7db2067131b286d5b89f4783ce7b0
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2023-03-16 12:03:14 -0500

    debug: extra echo

--- models/source.go
+++ models/source.go
@@ -28,10 +28,14 @@ func (h Source) GetSourceById(id int) (*Source, error) {
 func (h Source) GetSourceByIdWithConnection(id int) (*Source, error) {
 	db2 := db.GetAtlasDB().Db
 	var dataSource *Source
-	db2.Model(&Source{}).
+	result = db2.Model(&Source{}).
 		Select("source_id, source_name, source_connection, source_dialect, username, password").
 		Where("source_id = ?", id).
 		Scan(&dataSource)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
+	}
 	fmt.Println("c", "here")
 	fmt.Println("d", dataSource)
 	return dataSource, nil

90e5736b1e8dad81db2b067df7eaabeecddc0aab
tianj7 <jtian1@uchicago.edu>
2024-04-19 13:09:52 -0500

    dummy commit

--- server/router.go
+++ server/router.go
@@ -47,6 +47,7 @@ func NewRouter() *gin.Engine {
 
 		// cohort stats and checks:
 		cohortData := controllers.NewCohortDataController(*new(models.CohortData), *new(models.DataDictionary), middlewares.NewTeamProjectAuthz(*new(models.CohortDefinition), &http.Client{}))
+
 		// :casecohortid/:controlcohortid are just labels here and have no special meaning. Could also just be :cohortAId/:cohortBId here:
 		authorized.POST("/cohort-stats/check-overlap/by-source-id/:sourceid/by-cohort-definition-ids/:casecohortid/:controlcohortid", cohortData.RetrieveCohortOverlapStats)
 

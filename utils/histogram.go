3986632ba9fafc3ed0b4872ece4c9620e4cdf84f
tianj7 <jtian1@uchicago.edu>
2024-05-02 15:04:50 -0500

    update bin size

--- utils/histogram.go
+++ utils/histogram.go
@@ -14,7 +14,7 @@ type HistogramColumn struct {
 	NumberOfPeople int     `json:"personCount"`
 }
 
-const MAX_NUM_BINS = 100
+const MAX_NUM_BINS = 50
 
 func GenerateHistogramData(conceptValues []float64) []HistogramColumn {
 

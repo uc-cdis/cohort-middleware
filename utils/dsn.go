d9245304b161510ad84ff62f0052fed7179bf525
pieterlukasse <pieterlukasse@gmail.com>
2024-10-01 16:59:18 +0200

    feat: make db connection more generic; add larger dataset

--- utils/dsn.go
+++ utils/dsn.go
@@ -16,9 +16,22 @@ func GenerateDsn(sourceConnectionString string) string {
 	log.Printf("Found db vendor %s", dbVendor)
 	host := sourceConnectionParts[2]
 	port := sourceConnectionParts[3]
-	dbname := sourceConnectionParts[5]
-	username := sourceConnectionParts[7]
-	password := sourceConnectionParts[9]
+	dbname := ""
+	username := ""
+	password := ""
+	if len(sourceConnectionParts) == 9 {
+		// expecting a string like this: jdbc:postgresql://hostname.com:5432/dbname?user=username&etc
+		dbname = sourceConnectionParts[4]
+		username = sourceConnectionParts[6]
+		password = sourceConnectionParts[8]
+	} else if len(sourceConnectionParts) == 10 {
+		// expecting a string like this: jdbc:sqlserver://hostname.com:1433;databaseName=dbname;user=username;etc
+		dbname = sourceConnectionParts[5]
+		username = sourceConnectionParts[7]
+		password = sourceConnectionParts[9]
+	} else {
+		panic("connection string format not supported")
+	}
 
 	dsn := fmt.Sprintf(dbVendor+"://%s:%s@%s:%s?database=%s",
 		username,
